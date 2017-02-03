510311ae66f148e659a4298c3d33e42c85699daf
ffsommers <ssommers21@gmail.com>
2017-02-03 13:16:58 -0800

    adds jquery event listener to initialize generation of pdf.

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+$("#pdf-container").on("click",function(){
+  dronedeployApiInitialze()
+    .then(window.dronedeploy.Plans.getCurrentlyViewed)
+    .then(fetchTileDataFromPlan)
+    .then(getTilesFromResponse)
+    .then(getDataUrlViaCustomWebServer)
+    .then(generatePDF)
+    .catch(console.log);
+  
+}); 
+
 function fetchTileDataFromPlan(plan) {
   // assumed the default layerName as 'ortho' and zoom level as 16
   return window.dronedeploy.Tiles.get({planId: String(plan.id), layerName: 'ortho', zoom: 16});
