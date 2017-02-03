330ad9f396f26d5a2f442c60920e79c51bf995a5
ffsommers <ssommers21@gmail.com>
2017-02-03 13:13:52 -0800

    adds get tile functions from drone deploy api docs to scripts.js

---
+++ script.js
@@ -0,0 +1,24 @@
+function fetchTileDataFromPlan(plan) {
+  // assumed the default layerName as 'ortho' and zoom level as 16
+  return window.dronedeploy.Tiles.get({planId: String(plan.id), layerName: 'ortho', zoom: 16});
+}
+
+function getTilesFromResponse(tileResponse) {
+  return tileResponse.tiles;
+}
+
+function getDataUrlViaCustomWebServer(tiles) {
+  const webServerUrl = 'https://dronenode.herokuapp.com/tiles/';
+  const body = JSON.stringify({
+    'tile': tiles
+  });
+  return fetch(webServerUrl, {
+    method: 'POST',
+    headers : new Headers({
+        'Content-Type': 'application/json'
+    }),
+    body: body
+  })
+    .then((res) => res.json())
+    .then((rjson) => rjson);
+}
